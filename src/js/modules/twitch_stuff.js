48d744de29016dffcf16bc728c05a01f48797bb8
Vince Suriani <chenzorama@hotmail.com>
2020-04-29 14:59:29 -0400

    Twitch chat connected

--- src/js/modules/twitch_stuff.js
+++ src/js/modules/twitch_stuff.js
@@ -1,4 +1,6 @@
 //get twitch stuff
+
+import TwitchJS from 'twitch-js';
 import configData from './config.js';
 
 var twitch_obj = {
@@ -17,41 +19,111 @@ var twitch_obj = {
     },
 
     getLatestFollowers: function() {
-        fetch('https://api.twitch.tv/helix/users/follows?to_id=' + configData.userID, {
-          headers: {
-            'Client-ID': configData.Client_ID
-          }
-        })
-        .then(response => response.json())
-        .then(data =>
-          this.displayFollowers(data)
-        )
-        .catch(error => 
-          console.log("Twitch Fetch Errored: " + error)
-        );
-      },
+      fetch('https://api.twitch.tv/helix/users/follows?to_id=' + configData.userID, {
+        headers: {
+          'Client-ID': configData.Client_ID
+        }
+      })
+      .then(response => response.json())
+      .then(data =>
+        this.displayFollowers(data)
+      )
+      .catch(error => 
+        console.log("Twitch Fetch Errored: " + error)
+      );
+    },
+  
     
-    
-      getTwitchStreamerData: function() {
-        fetch('https://api.twitch.tv/helix/users?login=lirik', {
-          headers: {
-            'Client-ID': configData.Client_ID
-          }
-        })
-        .then(response => response.json())
-        .then(data =>
-          console.log(data)
-        )
-        .catch(error => 
-          console.log("Twitch Fetch Errored: " + error)
-        );
-      },
+    getTwitchStreamerData: function() {
+      fetch('https://api.twitch.tv/helix/users?login=lirik', {
+        headers: {
+          'Client-ID': configData.Client_ID
+        }
+      })
+      .then(response => response.json())
+      .then(data =>
+        console.log(data)
+      )
+      .catch(error => 
+        console.log("Twitch Fetch Errored: " + error)
+      );
+    },
 
-      init: function() {
-        console.log("Twitch INIT");
-        this.getLatestFollowers();
+
+    initTwitch() {
+      console.log('Activating Twitch');
+      const options = {
+        channels: ["#chenzorama"],
+        connection: {
+          secure: true
+        },
+        identity: {
+          username: "chenzorama",
+          password: configData.OAUTH
+        }
+      };
+  
+      const client = new TwitchJS.client(options);
+  
+      client.on('chat', (channel, userstate, message, self) => {
+        console.log(`Message "${message}" received from ${userstate['display-name']}`);
+        console.log(userstate);
+  
+  
+  
+        // Do not repond if the message is from the connected identity.
+        if (self) return;
+  
+        
+        if (options.identity && message.substring(0, 8) === '!addcrew') {
+          if (userstate['display-name'] == "Chenzorama" || userstate['mod']) {
+            const crewname = message.substr(9);//.split(" ")[0];
+            this.addCrew(crewname);
+            client.say(channel, crewname + ' added as crew member!');
+          } 
+        }
+  
+        if (options.identity && message.substring(0, 11) === '!removecrew') {
+          if (userstate['display-name'] == "Chenzorama" || userstate['mod']) {
+            console.log("removing")
+            const crewInt = message.substring(12);
+            const who = this.removeCrew(crewInt);
+            client.say(channel, who + ' removed as crew member!');
+          } 
+        }
+  
+        if (options.identity && message === '!command') {
+          // If an identity was provided, respond in channel with message.
+          client.say(channel, 'Hello world!');
+        }
+  
+        
+      });
+  
+      client.on('join', function(channel, username, self) {
+        //User Detected
+        console.log(username + " has joined the party"+ " | isSelf: " + self);
+      });
+  
+  
+      client.on('part', function(channel, username, self) {
+        //User Left
+        console.log(username + " has left the building" + " | isSelf: " + self);
+      });
+  
+      // Finally, connect to the channel
+      client.connect();
+      console.log('---Twitch Activated');
+    },
+
+
+
+
+    init: function() {
+      console.log("Twitch INIT");
+      this.getLatestFollowers();
+      this.initTwitch();
     }
-
 
 };
 
