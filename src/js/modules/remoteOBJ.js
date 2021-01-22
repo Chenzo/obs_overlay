e52e688b9244e6e139ab06c7ee1522d6b1e3123a
Vince Suriani <chenzorama@hotmail.com>
2021-01-22 16:06:38 -0500

    george

--- src/js/modules/remoteOBJ.js
+++ src/js/modules/remoteOBJ.js
@@ -80,6 +80,20 @@ export const remoteOBJ = (() => {
         }
     };
 
+    const romanize = function(num) {
+        if (isNaN(num))
+        return NaN;
+        var digits = String(+num).split(""),
+            key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                "","I","II","III","IV","V","VI","VII","VIII","IX"],
+            roman = "",
+            i = 3;
+        while (i--)
+            roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+        return Array(+digits.join("") + 1).join("M") + roman;
+    }
+
     const init = function() {
         console.log("SERVER: " + configData.server);
         socket = io(configData.server);
